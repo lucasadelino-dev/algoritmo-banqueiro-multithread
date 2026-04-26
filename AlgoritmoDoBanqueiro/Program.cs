b6638862a926174095d2895ae94570e4060be391
Lucas Adelino <lucasadelino.dev@gmail.com>
2026-04-26 12:30:57 -0300

    feat: adiciona configuração do arquivo program.cs

--- AlgoritmoDoBanqueiro/Program.cs
+++ AlgoritmoDoBanqueiro/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    const int NUMBER_OF_CUSTOMERS = 5;
+
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Uso: dotnet run -- 10 5 7");
+            return;
+        }
+
+        int[] recursos = new int[args.Length];
+
+        for (int i = 0; i < args.Length; i++)
+        {
+            recursos[i] = int.Parse(args[i]);
+        }
+
+        Banco banco = new Banco(NUMBER_OF_CUSTOMERS, recursos);
+
+        banco.MostrarEstado();
+
+        Thread[] threads = new Thread[NUMBER_OF_CUSTOMERS];
+
+        for (int i = 0; i < NUMBER_OF_CUSTOMERS; i++)
+        {
+            int id = i;
+
+            threads[i] = new Thread(() =>
+            {
+                Cliente cliente = new Cliente(id, banco);
+                cliente.Executar();
+            });
+
+            threads[i].Start();
+        }
+
+        foreach (Thread thread in threads)
+        {
+            thread.Join();
+        }
+    }
+}
