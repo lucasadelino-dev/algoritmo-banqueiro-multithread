bfc331a4a4babd1f5aeacfeaa84c2aa4539294e7
Lucas Adelino <lucasadelino.dev@gmail.com>
2026-04-26 12:22:02 -0300

    feat: adiciona entidade banco

---
+++ AlgoritmoDoBanqueiro/Banco.cs
@@ -0,0 +1,66 @@
+using System;
+
+public class Banco
+{
+    private int numeroClientes;
+    private int numeroRecursos;
+
+    private int[] available;
+    private int[,] maximum;
+    private int[,] allocation;
+    private int[,] need;
+
+    private object trava = new object();
+
+    public Banco(int numeroClientes, int[] recursosDisponiveis)
+    {
+        this.numeroClientes = numeroClientes;
+        this.numeroRecursos = recursosDisponiveis.Length;
+
+        available = new int[numeroRecursos];
+        maximum = new int[numeroClientes, numeroRecursos];
+        allocation = new int[numeroClientes, numeroRecursos];
+        need = new int[numeroClientes, numeroRecursos];
+
+        Array.Copy(recursosDisponiveis, available, numeroRecursos);
+
+        InicializarMaximum();
+        InicializarNeed();
+    }
+
+    private void InicializarMaximum()
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < numeroClientes; i++)
+        {
+            for (int j = 0; j < numeroRecursos; j++)
+            {
+                maximum[i, j] = random.Next(1, available[j] + 1);
+            }
+        }
+    }
+
+    private void InicializarNeed()
+    {
+        for (int i = 0; i < numeroClientes; i++)
+        {
+            for (int j = 0; j < numeroRecursos; j++)
+            {
+                need[i, j] = maximum[i, j];
+            }
+        }
+    }
+
+    public void MostrarEstado()
+    {
+        Console.WriteLine("Recursos disponíveis:");
+
+        for (int i = 0; i < numeroRecursos; i++)
+        {
+            Console.Write(available[i] + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
