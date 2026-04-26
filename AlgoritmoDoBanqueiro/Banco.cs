960c2eca241c8fe8c76447410a1b51ef3b90b232
Lucas Adelino <lucasadelino.dev@gmail.com>
2026-04-26 12:27:35 -0300

    feat: adiciona método de solicitar recursos

--- AlgoritmoDoBanqueiro/Banco.cs
+++ AlgoritmoDoBanqueiro/Banco.cs
@@ -65,27 +65,127 @@ public class Banco
     }
 
     public int ReleaseResources(int cliente, int[] release)
-{
-    lock (trava)
     {
+        lock (trava)
+        {
+            for (int i = 0; i < numeroRecursos; i++)
+            {
+                if (release[i] > allocation[cliente, i])
+                {
+                    return -1;
+                }
+            }
+
+            for (int i = 0; i < numeroRecursos; i++)
+            {
+                allocation[cliente, i] -= release[i];
+                available[i] += release[i];
+                need[cliente, i] += release[i];
+            }
+
+            Console.WriteLine($"Cliente {cliente} liberou recursos.");
+
+            return 0;
+        }
+    }
+
+    private bool IsSafe()
+    {
+        int[] work = new int[numeroRecursos];
+        bool[] finish = new bool[numeroClientes];
+
         for (int i = 0; i < numeroRecursos; i++)
         {
-            if (release[i] > allocation[cliente, i])
+            work[i] = available[i];
+        }
+
+        bool encontrou;
+
+        do
+        {
+            encontrou = false;
+
+            for (int i = 0; i < numeroClientes; i++)
             {
-                return -1;
+                if (!finish[i])
+                {
+                    bool podeExecutar = true;
+
+                    for (int j = 0; j < numeroRecursos; j++)
+                    {
+                        if (need[i, j] > work[j])
+                        {
+                            podeExecutar = false;
+                            break;
+                        }
+                    }
+
+                    if (podeExecutar)
+                    {
+                        for (int j = 0; j < numeroRecursos; j++)
+                        {
+                            work[j] += allocation[i, j];
+                        }
+
+                        finish[i] = true;
+                        encontrou = true;
+                    }
+                }
+            }
+
+        } while (encontrou);
+
+        for (int i = 0; i < numeroClientes; i++)
+        {
+            if (!finish[i])
+            {
+                return false;
             }
         }
 
-        for (int i = 0; i < numeroRecursos; i++)
-        {
-            allocation[cliente, i] -= release[i];
-            available[i] += release[i];
-            need[cliente, i] += release[i];
-        }
-
-        Console.WriteLine($"Cliente {cliente} liberou recursos.");
-
-        return 0;
+        return true;
     }
-}
+
+    public int RequestResources(int cliente, int[] request)
+    {
+        lock (trava)
+        {
+            for (int i = 0; i < numeroRecursos; i++)
+            {
+                if (request[i] > need[cliente, i])
+                {
+                    return -1;
+                }
+    
+                if (request[i] > available[i])
+                {
+                    return -1;
+                }
+            }
+    
+            for (int i = 0; i < numeroRecursos; i++)
+            {
+                available[i] -= request[i];
+                allocation[cliente, i] += request[i];
+                need[cliente, i] -= request[i];
+            }
+    
+            if (IsSafe())
+            {
+                Console.WriteLine($"Cliente {cliente} recebeu recursos.");
+                return 0;
+            }
+    
+            for (int i = 0; i < numeroRecursos; i++)
+            {
+                available[i] += request[i];
+                allocation[cliente, i] -= request[i];
+                need[cliente, i] += request[i];
+            }
+    
+            Console.WriteLine($"Cliente {cliente} teve pedido negado.");
+    
+            return -1;
+        }
+    }
 }
